c02646ac3ebd533a9b33393b5a071b74936c35bb
Abhinav Yadav <146636960+abhinav405@users.noreply.github.com>
2026-09-07 21:22:47 +0530

    Add files via upload

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,29 @@
+"use Strict"; /* treat js code as newer version */
+
+// alert(3 + 3) -> this will throw an error here bcz we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "Abhinav"
+let age = 5
+let isstudent = true
+let college;
+
+console.table([name,age,isstudent,college])
+
+console.log(typeof name )
+console.log(typeof age )
+console.log(typeof isstudent )
+console.log(typeof college ) // undefined
+console.log(typeof null ) // object
+
+/* DATATYPES
+1 Number - 2 to power 53
+2 String - ""
+3 Boolean - true/false
+4 Undefined 
+5 Null - standalone value
+6 bigint
+7 symbol - unique
+8 NaN
+*/ 
